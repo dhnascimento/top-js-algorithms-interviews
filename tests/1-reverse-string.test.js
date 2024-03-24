fa57839e36b2719fbdcaaf5fb78e6269e933514f
dhnascimento <danielhn@gmail.com>
2024-03-24 17:16:48 -0400

    code formatting

--- tests/1-reverse-string.test.js
+++ tests/1-reverse-string.test.js
@@ -3,7 +3,7 @@ const reverseString = require('../exercises/1-reverse-string');
 describe('reverseString', () => {
     test('reverseString reverses strings properly', () => {
         expect(reverseString('hello')).toBe('olleh');
-        expect(reverseString('hi')).toBe('ih')
+        expect(reverseString('hi')).toBe('ih');
         expect(reverseString('abba')).toBe('abba');
         expect(reverseString('DanielNascimento')).toBe('otnemicsaNleinaD');
     });
